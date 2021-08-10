8a948c4ba2624287cd8e2a839124f0eabe1f9337
mateusparr <mateus.dji@icloud.com>
2021-08-10 15:23:31 -0300

    inicio requisito

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import * as API from '../services/api';
 
 class SearchField extends React.Component {
   render() {
+    showFiltered = async () => {
+      const products = await API.getCategories;
+      console.log(products);
+    };
+
     return (
       <div className="search">
         <input type="text" />
