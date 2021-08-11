d6185bb691d036f3c8a20558d7bfbc70e8e3a696
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-08-11 19:10:54 -0300

    corrige lint

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -12,7 +12,7 @@ class ShoppingCart extends React.Component {
         <ShoppingCartIcon />
         { state.length === 0
           ? <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-          : state.map(({ id, title, thumbnail, price }) => (
+          : state.map(({ id, title, thumbnail }) => (
             <div key={ id }>
               <img src={ thumbnail } alt={ title } />
               <p data-testid="shopping-cart-product-name">{ title }</p>
