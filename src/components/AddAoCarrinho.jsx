d43d7943c8a54dfcb70a6581b57ac5683378e482
Alexander <alexanderteodorocastro95@hotmail.com>
2021-08-11 09:49:19 -0300

    Iniciando req 8

---
+++ src/components/AddAoCarrinho.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class AddAoCarrinho extends React.Component {
+  constructor(){
+    super();
+
+    AddCar() {
+      
+
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Button data-testid="product-add-to-cart">
+          Add ao Carrinho
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default AddAoCarrinho;
