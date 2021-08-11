7944ed0ec575770bba4bb9ba11fffeeca6283ff3
Itaopara Neto <itaopara@gmail.com>
2021-08-11 16:16:29 -0300

    Product Js

---
+++ src/components/Product.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+​
+class Product extends Component {
+  render() {
+    const { title, thumbnail, price } = this.props;
+    return (
+      <section data-testid="product">
+        <div>
+          <p>{ title }</p>
+          <img src={ thumbnail } alt={ title } />
+        </div>
+        <div>
+          <span>{ price }</span>
+        </div>
+      </section>
+    );
+  }
+}
+Product.propTypes = {
+  title: PropTypes.string.isRequired,
+  thumbnail: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
+export default Product;
