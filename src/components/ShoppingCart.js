34e5fc4bb33fde2612ecc8acd6cc3343b05c3fc8
Paulo Lima <paulynho_lima@hotmail.com>
2021-08-11 20:31:28 -0300

    refatorando quantidade do prodt

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -2,21 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import ShoppingCartIcon from './ShoppingCartIcon';
+// import { prettyDOM } from '@testing-library/dom';
 
 class ShoppingCart extends React.Component {
   render() {
     const { location: { state } } = this.props;
+
+    const quant = state.reduce((ac, corr) => {
+      const arry = ac;
+      if (corr.id in ac) {
+        corr.quantity += 1;
+      } else {
+        corr.quantity = 1;
+      }
+      arry.push(corr);
+      return arry;
+    }, []);
 
     return (
       <div>
         <ShoppingCartIcon />
         { state.length === 0
           ? <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-          : state.map(({ id, title, thumbnail }) => (
+          : state.map(({ id, title, thumbnail, quantity }) => (
             <div key={ id }>
               <img src={ thumbnail } alt={ title } />
               <p data-testid="shopping-cart-product-name">{ title }</p>
-              <p data-testid="shopping-cart-product-quantity">1</p>
+              <p data-testid="shopping-cart-product-quantity">{ quantity }</p>
             </div>
           ))}
       </div>
