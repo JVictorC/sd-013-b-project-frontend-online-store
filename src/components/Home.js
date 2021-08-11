2fa0fef941d0ab72c383e1928e008565052aaed1
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-08-11 17:24:01 -0300

    Metade requisito 8

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+//
 import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 import ShoppingCartIcon from './ShoppingCartIcon';
 import ProductList from './ProductList';
