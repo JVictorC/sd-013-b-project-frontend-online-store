ab0858b99880cdc141b0ecd963bc44ccff75309e
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-08-11 16:05:43 -0300

    Diminuindo conflitos

--- src/components/initial/ShoppingCart.jsx
+++ src/components/initial/ShoppingCart.jsx
@@ -45,7 +45,11 @@ class ShoppingCart extends Component {
         {productList.map((prod) => (
           <div key={ prod.id }>
             <span data-testid="shopping-cart-product-name">{` ${prod.title} `}</span>
-            <span data-testid="shopping-cart-product-quantity">{ prod.quantity }</span>
+            <span
+              data-testid="shopping-cart-product-quantity"
+            >
+              {` ${prod.quantity} `}
+            </span>
             <span>{ prod.price }</span>
           </div>
         ))}
