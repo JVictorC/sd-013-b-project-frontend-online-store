7b7b72de644e2851865cfb19055edc92fc4350ae
Pedro <pedrorsehn@hotmail.com>
2021-08-12 15:15:40 -0300

    alinhamento de linter

--- src/Pages/ShoppCart.js
+++ src/Pages/ShoppCart.js
@@ -4,24 +4,25 @@ class ShoppCart extends React.Component {
   render() {
     const cartItems = JSON.parse(localStorage.getItem('cart'));
     if (cartItems.length > 0) {
-      return (<div key="1" data-testid="shopping-cart-empty-message">
-        {cartItems.map((item) => {
-          const product = JSON.parse(item);
-          const { title, thumbnail, price } = product;
-          return (
-            <div key={ item.id } data-testid="product" className="productCard">
-              <h3 data-testid="shopping-cart-product-name" className="title">
-                { title }
-              </h3>
-              <img src={ thumbnail } alt="test" className="thumbnail" />
-              <h4 className="price">
-                {`R$ ${price}` }
-              </h4>
-              <p data-testid="shopping-cart-product-quantity">1</p>
-            </div>
-          );
-        })}
-      </div>);
+      return (
+        <div key="1" data-testid="shopping-cart-empty-message">
+          {cartItems.map((item) => {
+            const product = JSON.parse(item);
+            const { title, thumbnail, price } = product;
+            return (
+              <div key={ item.id } data-testid="product" className="productCard">
+                <h3 data-testid="shopping-cart-product-name" className="title">
+                  { title }
+                </h3>
+                <img src={ thumbnail } alt="test" className="thumbnail" />
+                <h4 className="price">
+                  {`R$ ${price}` }
+                </h4>
+                <p data-testid="shopping-cart-product-quantity">1</p>
+              </div>
+            );
+          })}
+        </div>);
     }
     return (
       <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
