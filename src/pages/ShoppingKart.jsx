ef05d8a720a99cb3e224d3c9f197ddf132b7abe4
Thiago <thiagodesouza.dev@gmail.com>
2021-08-12 15:35:17 -0300

    requisito 10

--- src/pages/ShoppingKart.jsx
+++ src/pages/ShoppingKart.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import CartItem from '../components/CartItem';
+import homeIcon from '../img/homeIcon.png';
 
 class ShoppingKart extends Component {
   render() {
+    const { cart } = this.props;
+    if (!cart.length) {
+      return (
+        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+      );
+    }
     return (
       <div>
-        <span data-testid="shopping-cart-empty-message">Seu carrinho está vazio</span>
+        <Link to="/">
+          <img
+            src={ homeIcon }
+            alt="Voltar para página principal"
+          />
+        </Link>
       </div>
     );
   }
