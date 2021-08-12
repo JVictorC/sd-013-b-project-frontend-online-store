f6364caaf8e4b192d494a9f81077dd53be904c45
Camila Dornas <camiladornas@gmail.com>
2021-08-12 15:41:41 -0300

    Andamento requisito 8

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -2,14 +2,41 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class ProductCard extends Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    const selectProduct = event.target.parentElement.previousElementSibling.innerText;
+    console.log(selectProduct);
+    // const resolve = await fetch(`https://api.mercadolibre.com/items/${id}`);
+    // const data = await resolve.json();
+    // const ol = document.querySelector('ol');
+    // const creatItem = createCartItemElement(data);
+    // ol.appendChild(creatItem);
+  }
+
   render() {
     const { product } = this.props;
     const { title, price, thumbnail } = product;
     return (
       <div data-testid="product" className="produto">
-        <h2>{ title }</h2>
-        <h4>{ price }</h4>
-        <img alt="Product disc" src={ thumbnail } />
+        <div>
+          <h2>{ title }</h2>
+          <h4>{ price }</h4>
+          <img alt="Product disc" src={ thumbnail } />
+        </div>
+        <div>
+          <button
+            type="button"
+            data-testid="product-add-to-cart"
+            onClick={ this.handleClick }
+            id="product-add-to-cart"
+          >
+            Adicionar ao carrinho
+          </button>
+        </div>
       </div>
     );
   }
