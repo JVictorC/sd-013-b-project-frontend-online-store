487206f8f8b805e5a6bb64cb249c60a0df3dbc10
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-08-12 02:32:15 -0300

    Move components

---
+++ src/pages/SearchBar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  render() {
+    const { query, handleChange, handleClick } = this.props;
+
+    return (
+      <div>
+        <input
+          type="text"
+          data-testid="query-input"
+          name="query"
+          value={ query }
+          onChange={ handleChange }
+        />
+        <button
+          type="submit"
+          data-testid="query-button"
+          onClick={ handleClick }
+        >
+          Pesquisar
+        </button>
+      </div>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  query: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  handleClick: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
