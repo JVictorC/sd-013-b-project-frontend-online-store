6da2cc347de1eb641a2c5739a3d9b3d2915652a2
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-08-12 15:52:29 -0300

    Adiciona seção revisar produtos.

--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 class Checkout extends Component {
   render() {
+    const cartItems = JSON.parse(localStorage.getItem('card'));
     return (
       <div className="checkout">
         <Link to="/">🏠</Link>
@@ -10,6 +11,24 @@ class Checkout extends Component {
         <main className="container">
           <section>
             <h2>Revise seus Produtos</h2>
+            <ol>
+              {cartItems.map((item) => (
+                <li key={ item.id }>
+                  <h3>{item.title}</h3>
+                  <img src={ item.thumbnail } alt="produto" />
+                  <h3>
+                    <strong>{item.price}</strong>
+                  </h3>
+                </li>
+              ))}
+            </ol>
+            <h2>
+              Total a pagar:
+              {' '}
+              {`R$ ${cartItems
+                .reduce((total, item) => total + item.price, 0)
+                .toFixed(2)}`}
+            </h2>
           </section>
           <section>
             <h2>Informações do Comprador</h2>
@@ -18,7 +37,6 @@ class Checkout extends Component {
             <h2>Métodos de Pagamento</h2>
           </section>
         </main>
-
       </div>
     );
   }
