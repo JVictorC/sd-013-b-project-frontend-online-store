123a4e1ba975eb33853fb30bf2235358a8123415
EddieRAM <edusacarneiro@gmail.com>
2021-08-12 16:48:06 -0300

    Requisito 7

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CarrinhoDeCompra from './pages/CarrinhoDeCompra';
 import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Route exact path="/" component={ Home } />
         <Route path="/cart" component={ CarrinhoDeCompra } />
+        <Route path="/details/:id" component={ ProductDetails } />
       </Router>
     </div>
   );
