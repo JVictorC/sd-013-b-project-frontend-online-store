852d8d7016cb948a205eb3052bea765d5b7939cc
Alessandro Hugen <alessandrohugen@gmail.com>
2021-08-12 17:44:44 -0300

    Requisito 8

--- src/Componentes/ProductList.jsx
+++ src/Componentes/ProductList.jsx
@@ -3,12 +3,22 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    localStorage.setItem('item', JSON.stringify(event));
+  }
+
   render() {
     const { products } = this.props;
     return (
       products ? products.map((product) => (
         <div data-testid="product" key={ product.id }>
-          <h5>{product.title}</h5>
+          <h5>{ product.title }</h5>
           <Link
             data-testid="product-detail-link"
             to={ `/details/${encodeURIComponent(product.title)}` }
@@ -17,6 +27,13 @@ class ProductList extends React.Component {
           </Link>
           <img src={ product.thumbnail } alt={ product.title } width="150px" />
           <p>{`R$${product.price}`}</p>
+          <button
+            type="button"
+            data-testid="product-add-to-cart"
+            onClick={ () => this.handleClick(product) }
+          >
+            Adicionar ao Carrinho
+          </button>
         </div>
       )) : <p>Nao existe produtos</p>
     );
