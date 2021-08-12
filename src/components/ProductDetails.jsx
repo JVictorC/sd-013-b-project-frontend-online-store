9d62d107339030fb9474da46cccd0673abeee849
Pedro Brito <pedroab0119@gmail.com>
2021-08-11 22:28:16 -0300

    req8 lint

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -2,47 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import shoppingCart from '../images/shopping-cart-svgrepo-com.svg';
-import * as api from '../services/api';
-// import Loading from './Loading';
 
 class ProductDetails extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      productDetail: [],
-      loaded: false,
-    };
-
-    // this.getProductDetails = this.getProductDetails.bind(this);
-  }
-
-  // componentDidMount() {
-  //   this.getProductDetails();
-  // }
-
-  // async getProductDetails() {
-  //   const { match: { params: { id } } } = this.props;
-  //   const product = await api.getProductsFromCategoryAndQuery({ id });
-  //   if (product !== this.state.productDetail ) {
-  //     console.log(product);
-  //     this.setState = {
-  //       productDetail: product,
-  //       loaded: true,
-  //     };
-  //   }
-  // }
-
   render() {
-    // if (this.state.loaded) {
-    //   console.log(this.state.productDetail);
-    //   return (
-    //     <div data-testid="product-detail-name" >
-    //       if
-    //     </div>
-    //   );
-    // };
-
-    const { state: productDetail } = this.props.location;
+    const { location } = this.props;
+    const { state: productDetail } = location;
     return (
       <div>
         <div className="product-header">
@@ -68,7 +32,8 @@ class ProductDetails extends React.Component {
           <div className="espec-product">
             <div className="title-specs">especificações técnicas</div>
             <div className="every-spec">
-              { productDetail.attributes.map((spec) => <p key={ spec.id }>{spec.name}</p>)}
+              { productDetail
+                .attributes.map((spec) => <p key={ spec.id }>{spec.name}</p>)}
             </div>
           </div>
         </div>
