ef92012862a5e45aa6a6dc2285cc8b2132081565
YasminAraujo3 <yasmin.nunesaraujo@gmail.com>
2021-08-11 21:18:18 -0300

    Requisito 8 feito e consertando o requisito 6

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class ProductCard extends React.Component {
+  constructor() {
+    super();
+    this.addHandler = this.addHandler.bind(this);
+  }
+
+  addHandler() {
+    const { product, onAdd } = this.props;
+    onAdd(product);
+  }
+
   render() {
     const { product: { title, thumbmail, price } } = this.props;
 
@@ -14,6 +24,13 @@ class ProductCard extends React.Component {
         <h4>
           { price }
         </h4>
+        <button
+          type="button"
+          onClick={ this.addHandler }
+          data-testid="product-add-to-cart"
+        >
+          Adicionar ao Carrinho
+        </button>
       </div>
     );
   }
@@ -25,6 +42,7 @@ ProductCard.propTypes = {
     thumbmail: PropTypes.string,
     price: PropTypes.number,
   }).isRequired,
+  onAdd: PropTypes.func.isRequired,
 };
 
 export default ProductCard;
--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,15 +1,57 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ShoppingCartButton from '../components/ShoppingCartButton';
 
 class ShoppingCart extends React.Component {
   render() {
+    const { cart } = this.props;
+    const finalCart = [];
+    console.log(cart);
+    cart.forEach((product, index) => {
+      let count = 1;
+      cart.forEach((product2, index2) => {
+        if (product.id === product2.id && index !== index2) {
+          count += 1;
+        }
+      });
+      const findProduct = finalCart.find((el) => el.data.id === product.id);
+      if (Math.abs(findProduct) === 1 || findProduct === undefined) {
+        finalCart.push({
+          data: product,
+          quantity: count,
+        });
+      }
+    });
+    console.log(finalCart);
     return (
       <div>
         <ShoppingCartButton />
-        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        {
+          cart.length > 0 ? (
+            finalCart.map((product) => (
+              <div key={ product.data.id }>
+                <span data-testid="shopping-cart-product-name">
+                  { product.data.title }
+                </span>
+                <b data-testid="shopping-cart-product-quantity">{ product.quantity }</b>
+              </div>
+            ))
+          ) : (
+            <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+          )
+        }
       </div>
     );
   }
 }
+
+ShoppingCart.propTypes = {
+  cart: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      title: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
 export default ShoppingCart;
