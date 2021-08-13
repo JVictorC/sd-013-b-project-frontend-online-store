30934aad1af04f6c9428e6a742038972da968d5b
MilenaCLeme <milenaleme4@hotmail.com>
2021-08-13 00:58:26 -0300

    Alteração carrinhoDeCompra

--- src/pages/CarrinhoDeCompra.js
+++ src/pages/CarrinhoDeCompra.js
@@ -1,25 +1,53 @@
 import React from 'react';
+import CartList from '../components/CartList';
 
 class CarrinhoDeCompra extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      products: this.props.products,
       mensagem: true,
+      value: {},
     };
   }
 
+  componentDidMount() {
+    this.valeuList();
+  }
+
+  valeuList() {
+    const { products } = this.state;
+    if (products.length > 0) {
+      this.setState({
+        products,
+        mensagem: false,
+      })
+    }
+  }
+
+  
+
   render() {
-    const { mensagem } = this.state;
+    const { mensagem, products } = this.state;
     if (mensagem) {
       return (
-        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        <div>
+          <h1>Carrinho De Compra</h1>
+          <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        </div>
       );
     }
 
     return (
-      <>
-      </>
+      <section>
+        <div>
+          <h1>Carrinho De Compra</h1>
+        </div>
+      {products.map((obj) => (
+        <CartList key={ obj.id } obj={ obj } />
+      ))}
+      </section>
     );
   }
 }
