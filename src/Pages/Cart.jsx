ae7d37edcd3e2055abb32584b4abb46aff5b9686
Gustavo Fernando <gu.bressan@outlook.com>
2021-08-12 19:01:15 -0700

    alo

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -9,14 +9,6 @@ class Cart extends React.Component {
     };
   }
 
-  calculation = ({ target }) => {
-    if (target.id === 'add') {
-      this.toAdd();
-    } else if (target.id === 'decrease') {
-      this.toRemove();
-    }
-  }
-
   toAdd = () => {
     this.setState((prevState) => ({ quanti: prevState.quanti + 1 }));
   }
@@ -25,59 +17,67 @@ class Cart extends React.Component {
     this.setState((prevState) => ({ quanti: prevState.quanti - 1 }));
   }
 
-  render() {
-    const { quanti } = this.state;
-    const { products } = this.props;
+    calculation = ({ target }) => {
+      if (target.id === 'add') {
+        this.toAdd();
+      } else if (target.id === 'decrease') {
+        this.toRemove();
+      }
+    }
 
-    if (products.length > 0) {
+    render() {
+      const { quanti } = this.state;
+      const { products } = this.props;
+
+      if (products.length > 0) {
+        return (
+          <div>
+            { products.map((product) => (
+              <div key={ product.id }>
+                <p data-testid="shopping-cart-product-name">{ product.title }</p>
+                <p>{ product.price }</p>
+                <img src={ product.thumbnail } alt="" />
+                <p>
+                  Quantidade:
+                  <span
+                    data-testid="shopping-cart-product-quantity"
+                  >
+                    {quanti}
+                  </span>
+                  <button
+                    type="button"
+                    data-testid="product-increase-quantity"
+                    onClick={ this.calculation }
+                    id="add"
+                  >
+                    +
+                  </button>
+                  <button
+                    type="button"
+                    data-testid="product-decrease-quantity"
+                    id="decrease"
+                    onClick={ this.calculation }
+                  >
+                    -
+                  </button>
+                </p>
+              </div>
+            )) }
+            <div>
+              <h1>
+                Valor total da compra:$
+              </h1>
+            </div>
+          </div>
+        );
+      }
+
       return (
         <div>
-          { products.map((product) => (
-            <div key={ product.id }>
-              <p data-testid="shopping-cart-product-name">{ product.title }</p>
-              <p>{ product.price }</p>
-              <img src={ product.thumbnail } alt="" />
-              <p>
-                Quantidade:
-                <span
-                  data-testid="shopping-cart-product-quantity"
-                >
-                  {quanti}
-                </span>
-                <button
-                  type="button"
-                  data-testid="product-increase-quantity"
-                  onClick={ this.calculation }
-                  id="add"
-                >
-                  +
-                </button>
-                <button
-                  type="button"
-                  data-testid="product-decrease-quantity"
-                  id="decrease"
-                  onClick={ this.calculation }
-                >
-                  -
-                </button>
-              </p>
-            </div>
-          )) }
-          <div>
-            <h1>
-              Valor total da compra:$
-            </h1>
-          </div>
+          <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
         </div>
       );
     }
-
-    return (
-      <div>
-        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-      </div>
-    );
-  }
 }
 
 Cart.propTypes = {
