2e6bfbbde463ce9b4367b46422b10ab1e6e586c4
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-08-12 22:06:35 -0300

    test

--- src/Components/ButtonCart.jsx
+++ src/Components/ButtonCart.jsx
@@ -9,6 +9,20 @@ export default class ButtonCart extends Component {
     if (Object.keys(this.props).length === 0) {
       return <Link data-testid="shopping-cart-button" to="/shopCart">Cart</Link>;
     }
+    const { productDetail } = this.props;
+    if (productDetail === 'productDetail') {
+      const { product, handleCartItems } = this.props;
+      return (
+        <button
+          data-testid="product-detail-add-to-cart"
+          type="button"
+          cartProducts={ product }
+          onClick={ () => handleCartItems(product) }
+        >
+          Adicionar ao carrinho
+        </button>
+      );
+    }
     const { product, handleCartItems } = this.props;
     return (
       <button
@@ -26,4 +40,5 @@ export default class ButtonCart extends Component {
 ButtonCart.propTypes = {
   product: PropTypes.objectOf(PropTypes.object).isRequired,
   handleCartItems: PropTypes.func.isRequired,
+  productDetail: PropTypes.string.isRequired,
 };
