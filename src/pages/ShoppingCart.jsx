5a93f5590dbffcfc6a81add1063e0a888977c8b5
marcus <marcus.rodrigues10@hotmail.com>
2021-08-13 11:52:48 -0300

    iniciando requisito 10

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -2,16 +2,56 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class ShoppingCart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quantity: 1,
+    };
+    this.increaseQtd = this.increaseQtd.bind(this);
+    this.decreaseQtd = this.decreaseQtd.bind(this);
+  }
+
+  increaseQtd = () => {
+    this.setState((prevState) => ({ quantity: prevState.quantity + 1 }));
+  }
+
+  decreaseQtd = () => {
+    const { quantity } = this.state;
+    if (quantity > 1) {
+      this.setState((prevState) => ({ quantity: prevState.quantity - 1 }));
+    }
+  }
+
   render() {
     const { location } = this.props;
     const { cart } = location;
+    const { quantity } = this.state;
     return (
       <div>
         <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
         {cart.map((car) => (
           <div key={ car.id }>
-            <p data-testid="shopping-cart-product-name">{car.title}</p>
-            <p data-testid="shopping-cart-product-quantity">1</p>
+            <h5 data-testid="shopping-cart-product-name">{car.title}</h5>
+            <p
+              data-testid="shopping-cart-product-quantity"
+              onChange={ this.increaseQtd }
+            >
+              { quantity }
+            </p>
+            <button
+              data-testid="product-increase-quantity"
+              type="button"
+              onClick={ this.increaseQtd }
+            >
+              +
+            </button>
+            <button
+              data-testid="product-decrease-quantity"
+              type="button"
+              onClick={ this.decreaseQtd }
+            >
+              -
+            </button>
           </div>
         ))}
       </div>
