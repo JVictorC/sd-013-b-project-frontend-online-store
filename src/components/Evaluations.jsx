f354840b1390178d3a640a340030e84b6d4887ab
Letícia Castro <aicitelks@gmail.com>
2021-08-13 09:10:27 -0300

    Correções liner <Evaluations />

--- src/components/Evaluations.jsx
+++ src/components/Evaluations.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 
 export default class Evaluations extends React.Component {
-
   render() {
-
     const object = JSON.parse(localStorage.getItem('currentState'));
 
     return (
       <div>
         {
           localStorage.length === 0 ? ''
-            : object.map(obj => (
+            : object.map((obj) => (
               <div>
-                <p>E-mail: {obj.email}</p>
-                <p>Nota: {obj.rating} </p>
-                <p>Mensagem: {obj.message}</p>
+                <p>
+                  E-mail: {obj.email}
+                </p>
+                <p>
+                  Nota: {obj.rating}
+                </p>
+                <p>
+                  Mensagem: {obj.message}
+                </p>
                 <hr />
               </div>
             ))
