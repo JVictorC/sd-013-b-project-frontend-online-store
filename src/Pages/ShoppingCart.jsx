754710a6333829e99d187105f53e0eab6ebe15d2
unknown <brunodevmoraes@gmail.com>
2021-08-14 14:41:21 -0300

    Adiciona elemento com quantidade de cada item

--- src/Pages/ShoppingCart.jsx
+++ src/Pages/ShoppingCart.jsx
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class ShoppingCart extends Component {
+  constructor() {
+    super();
+    this.countProducts = this.countProducts.bind(this);
+  }
+
+  countProducts(arr, product) {
+    const oneProductArr = arr.filter((object) => product.title === object.title);
+    return oneProductArr.length;
+  }
+
   render() {
     const { location: { data } } = this.props;
-    const cartRendering = data.map((product) => (
-      <div key={ product.id }>
-        <h3 data-testid="shopping-cart-product-name">{product.title}</h3>
-        <h5>{product.price}</h5>
-      </div>
-    ));
+    const cartRendering = data.map((product) => {
+      const quantity = this.countProducts(data, product);
+      return (
+        <div key={ product.id }>
+          <h3 data-testid="shopping-cart-product-name">{product.title}</h3>
+          <h5 data-testid="shopping-cart-product-quantity">{ quantity }</h5>
+          <h5>{product.price}</h5>
+        </div>);
+    });
     const em = <h3 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h3>;
     return (
       <div>
