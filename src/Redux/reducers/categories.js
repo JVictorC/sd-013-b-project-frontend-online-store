9f1a39eff227e60ea3b2c3371e05234d2de7809c
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-08-29 17:22:44 -0300

    reafatora reducers

--- src/Redux/reducers/categories.js
+++ src/Redux/reducers/categories.js
@@ -1,17 +1,15 @@
-import { SET_CATEGORIES } from '../actions'
+import { SET_CATEGORIES } from '../actions';
 
-const INITIAL_STATE = {
-  categories: [],
-}
+const INITIAL_STATE = [];
 
-const categories = (state = INITIAL_STATE, action) {
+const categories = (state = INITIAL_STATE, action) => {
   const { type, payload } = action;
-  switch (action.types) {
-    case SET_CATEGORIES:
-      return { }
-    default:
-      return state;
+  switch (type) {
+  case SET_CATEGORIES:
+    return [...state, ...payload];
+  default:
+    return state;
   }
-}
+};
 
 export default categories;
