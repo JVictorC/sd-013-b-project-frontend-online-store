f4baf1a5aa8f71ee074716ee1d1328d8a6472444
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-08-29 16:41:28 -0300

    implementa fetch gategory e query

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,13 @@ export async function getCategories() {
   // Implemente aqui
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(
+  categoryId = '$CATEGORY_ID',
+  query = '$QUERY',
+) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const API = 'https://api.mercadolibre.com/sites/MLB/search?category=';
+  const response = await fetch(`${API}${categoryId}&q=${query}`);
+  const data = await response.json(response);
+  return data;
 }
